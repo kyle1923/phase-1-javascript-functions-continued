b2ff17b246d71f70f329b94440ff29cc1176f836
Kyle Vermillion <kvermillion23@gmail.com>
2022-06-29 20:12:51 -0400

    commit tree 20bd41ee13e3c282ce5fc67f8baf7d882b7b8d83

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(message="roller-skate") {
+    return `This Saturday, I want to ${message}!`
+}
+    
+function mondayWork(message="go to the office") {
+  
+    return `This Monday, I will ${message}.`
+}
+    
+function wrapAdjective(wrap = "*") {
+    
+    
+    return function(word="special") {
+        return `You are ${wrap}${word}${wrap}!`
+    }  
+}
